eeb19acece38838ea94feba297f6711061750282
SANGJUN PARK <sangjun@SANGJUNs-MacBook-Pro.local>
2025-02-18 16:04:55 +0900

    init: Project Setting

---
+++ src/main/java/com/example/springnewbie/SpringnewbieApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnewbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnewbieApplication.class, args);
+	}
+
+}
